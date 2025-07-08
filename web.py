aab058e1b3b0494efa8028ceaab006ffd598df81
Rafael Barbosa <rafaelb.barbosa@gmail.com>
2025-07-08 16:39:37 -0300

    Adicionando app flask

---
+++ web.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from form_filler import LogzzFormFiller
+from browser import Browser
+
+app = Flask(__name__)
+
+@app.route('/preencher', methods=['POST'])
+def preencher():
+    dados_cliente = request.json
+    browser = Browser(headless=True)
+    filler = LogzzFormFiller(browser)
+    sucesso = filler.fill_stage_one(dados_cliente)
+    return jsonify({"sucesso": sucesso})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=50001)
